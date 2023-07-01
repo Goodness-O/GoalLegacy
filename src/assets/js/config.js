96e91f991d8dac6b4820861752e20e286c50cc01
Akpomena <akpomenaojefia@gmail.com>
2023-07-01 13:46:32 +0100

    Moe deletions

--- src/assets/js/config.js
+++
@@ -1,6 +0,0 @@
-import '../styles/base.scss';
-import '../styles/loader.scss';
-import '../styles/landing.scss';
-import '../styles/animatelink.scss';
-import '../styles/sneekpeeks.scss';
-import '../styles/sections.scss';
